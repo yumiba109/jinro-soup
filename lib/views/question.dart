de72fc779f4046cdfd79bd09adfa6ea828d22c80
yumiba109 <k.yumiba109@gmail.com>
2021-03-07 22:37:35 +0900

    問題を表示

--- lib/views/question.dart
+++ lib/views/question.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:jinro_soup/main.dart';
 
 class QuestionView extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
               child: _displayMaster(context),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              child: _displayQuestion(),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              child: const Text('答えを見る'),
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(Size(140, 40)),
+              ),
+              onPressed: () {},
             ),
           ],
         ),
@@ -32,6 +51,20 @@ class QuestionView extends HookWidget {
           ),
         ),
         Text('さんです'),
+      ],
+    );
+  }
+
+  Widget _displayQuestion() {
+    final questionState = useProvider(questionViewModelProvider.state);
+    final _questionList = questionState.questionList;
+
+    return Column(
+      children: <Widget>[
+        Text(
+          _questionList[0].question,
+          style: TextStyle(fontSize: 20),
+        ),
       ],
     );
   }
