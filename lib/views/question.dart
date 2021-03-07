9983e38576ad46ab42823c898e2907140846f647
yumiba109 <k.yumiba109@gmail.com>
2021-03-07 19:28:53 +0900

    マスターを表示

--- lib/views/question.dart
+++ lib/views/question.dart
@@ -5,7 +5,34 @@ class QuestionView extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              child: _displayMaster(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _displayMaster(BuildContext context) {
+    final String _masterName = ModalRoute.of(context).settings.arguments;
+
+    return Column(
+      children: <Widget>[
+        Text('今回のマスターは'),
+        Text(
+          _masterName,
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text('さんです'),
+      ],
     );
   }
 }
